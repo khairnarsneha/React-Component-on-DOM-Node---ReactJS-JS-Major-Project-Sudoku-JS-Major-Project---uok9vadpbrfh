3fcf4db066d0091dfc2867f8ac697a4e725b100a
Sneha Hari Khairnar <109480526+khairnarsneha@users.noreply.github.com>
2022-12-24 22:32:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-ReactDOM.render(<App />, document.getElementById('root'));
-/* Make App component a functional component */
-import React from 'react';
-import ReactDOM from 'react-dom';
-const App = () => {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
-};
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
